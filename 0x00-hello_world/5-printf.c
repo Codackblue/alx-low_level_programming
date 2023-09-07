4607086b6cd9fa743f0b85ceaf44f04e3e07526d
Codackblue <goodyokorafor1@gmail.com>
2023-09-07 11:30:56 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - my program that printsf
- * Return: 0 (done)
+ * Return: 0 (success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-i}
+}
