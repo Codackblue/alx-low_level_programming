f17204903fcad1cbe14b1aff6090155aaddd2266
Codackblue <goodyokorafor1@gmail.com>
2023-09-07 11:22:31 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - my program that printsf
  * Return: 0 (done)
- * */
+ */
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+i}
