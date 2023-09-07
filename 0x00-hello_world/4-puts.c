71705dc7e68c094a92594ba09c8e822fb14e0e7c
Codackblue <goodyokorafor1@gmail.com>
2023-09-07 09:54:48 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 
 /**
  * main - this is my function
+ *
  * Return: always 0
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle):
+	puts("\"Programming is like building a multilingual puzzle"):
 		return (0);
 }
