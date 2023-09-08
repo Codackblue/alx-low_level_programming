5b4cfd0a6093396e86c571a8185bee6f035ca945
Codackblue <goodyokorafor1@gmail.com>
2023-09-07 18:07:32 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - a program for quotes
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1)
 }
