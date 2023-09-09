386647189789a651f52e13897aea1b6b22af9fc5
Codackblue <goodyokorafor1@gmail.com>
2023-09-09 06:07:22 -0700

    field

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - putchar printing
+*
+*Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar('0' + numbers[i]);
+	}
+	putchar('\n');
+return (0);
+}
