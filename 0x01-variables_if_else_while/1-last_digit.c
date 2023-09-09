4d2ef2cdebbb7a5ad871b17a602275782bc8f4ce
Codackblue <goodyokorafor1@gmail.com>
2023-09-09 00:53:42 -0700

    module

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
-  * main - first time
-  * Return: Always 0
-  */
+*main - my first code for c prhramming
+*Return:  always 0 (success)
+ */
+int main(void)
+{
+	int n;
 
-int main(void) {
-	 int n;
-
-	 srand(time(0));
-	 n = rand() - RAND_MAX / 2;
-	
-	 int lastDigit = abs(n) % 10;
-
-	 printf("Random number: %d\n", n);
-	 if (lastDigit > 5) {
-	printf("and is greater than 5\n");
-	 } else if (lastDigit == 0) {
-	printf("and is 0\n");
-	 } else {
-	printf("and is less than 6 and not 0\n");
-	 }
-	 return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) > 5)
+	{
+	printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+	printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else
+	{
+	printf("last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	return (0);
 }
