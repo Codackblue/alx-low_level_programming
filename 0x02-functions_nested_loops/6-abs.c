dc869437ef7cd426b2da6f3c9b945691b5b90b6c
Codackblue <goodyokorafor1@gmail.com>
2023-09-12 13:43:12 -0700

    serial

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - produces the absolute value
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
