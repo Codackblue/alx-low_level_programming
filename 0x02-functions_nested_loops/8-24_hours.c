949adff4eddc32124816ebdcb46fd3ec13412ad1
Codackblue <goodyokorafor1@gmail.com>
2023-09-12 17:32:59 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints jack everytime
- * starting from 00:00 to 23:59
+ * jack_bauer - print everytime
+ *
+ * Return: void
  */
 void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{
-		j = 0;
-		while (j < 60)
-			_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
-		_putchar((j / 10) + '0');
-		_putchar((j % 10) + '0');
-		_putchar('\n');
-		j++;
+		for (j = 0; j < 60; j++)
 		{
-	i++;
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
-}
-}
+
