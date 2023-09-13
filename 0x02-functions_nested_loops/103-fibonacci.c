bdc4f4a8991af8659e1c6b2da59e9d29ae3b2bff
Codackblue <goodyokorafor1@gmail.com>
2023-09-12 20:07:40 -0700

    done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints fibonacci 
+ * Return: 0.always
+ */
+int main(void)
+{
+int sum = 0, x = 1, y = 2, temp;
+while (x <= 4000000)
+{
+if (x % 2 == 0)
+sum += x;
+temp = x;
+x = y;
+y += temp;
+}
+printf("%d\n", sum);
+return (0);
+}
