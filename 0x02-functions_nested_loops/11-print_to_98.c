3df4b6abc37707eea5c32acba0ec23649480869b
Codackblue <goodyokorafor1@gmail.com>
2023-09-12 18:54:55 -0700

    battery

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural
+ * followed by a new line
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+	int x, y;
+
+	if (n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x != 98)
+				printf("%d", x);
+			if (x == 98)
+				printf("%d", x);
+			printf(",");
+			 printf(" ");
+		}
+	}
+	else if (n >= 98)
+	{
+		for (y = n; y >= 98; y--)
+		{
+			if (y != 98)
+				printf("%d", y);
+			if (y == 98)
+				printf("%d\n", y);
+			printf(",");
+			 printf(" ");
+		}
+	}
+}
+
