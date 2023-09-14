f8f8c51c43b9eb651df48047a2f1e9d37e8a7fea
Codackblue <goodyokorafor1@gmail.com>
2023-09-14 13:30:35 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - my function that prints mul
+ * @a: first odered interger
+ * @b: next phase
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
