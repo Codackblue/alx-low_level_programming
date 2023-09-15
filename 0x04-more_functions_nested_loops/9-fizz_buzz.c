f3f060c533659e7f3fa3f268843c347c3922c499
Codackblue <goodyokorafor1@gmail.com>
2023-09-15 07:30:50 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - writes the character
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 5 == 0 && i % 3 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+printf("Buzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else
+printf("%d", i);
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
