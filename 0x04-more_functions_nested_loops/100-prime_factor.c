9fe66ef9f132bc72c21c40fb399354b8fa91cc2e
Codackblue <goodyokorafor1@gmail.com>
2023-09-15 06:08:15 -0700

    done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prime factors of 1231952
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, n, d;
+
+	n = 612852475143;
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			if (n == i)
+			{
+				printf("%ld\n", i);
+				break;
+			}
+			d = n / i;
+			n = d;
+		}
+	}
+	return (0);
+}
