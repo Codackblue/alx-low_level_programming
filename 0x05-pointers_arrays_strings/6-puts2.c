7d159adabe6e20d7fd009bb1c4c54b4ba1b98a2f
Codackblue <goodyokorafor1@gmail.com>
2023-09-18 09:25:38 -0700

    done

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out 
+ * followed by a new line down
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int len = 0, s = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	while (s < len)
+	{
+		_putchar(str[s]);
+		s += 2;
+	}
+	_putchar('\n');
+}
