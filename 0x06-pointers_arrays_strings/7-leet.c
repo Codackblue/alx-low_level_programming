52a8a477ff678c89c800d6db7b997d5db8784ede
Codackblue <goodyokorafor1@gmail.com>
2023-09-20 14:50:48 -0700

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
 /**
- * leet - function that encodes for me
+ * leet -code that prints and encodes foe me
  * @n: input value for me
  * Return: n value
  */
 char *leet(char *n)
 {
 	int i, j;
-
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\n'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; s1[j] != '\0'; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
-				break;
 			}
 		}
 	}
