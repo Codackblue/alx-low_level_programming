c2906cc9753c3a3945609b943403b740a16c587d
Codackblue <goodyokorafor1@gmail.com>
2023-09-21 08:45:54 -0700

    done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rot13 - add and encode the strinhs
+ * @s: string to be added.
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (a[j] != '\0')
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
