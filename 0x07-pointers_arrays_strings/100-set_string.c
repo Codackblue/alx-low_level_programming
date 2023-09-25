ec7c735f2459c843033905447d59a702a329b6b2
Codackblue <goodyokorafor1@gmail.com>
2023-09-25 14:05:41 -0700

    done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value for the pointer tk match the string
+ * @s: pointer to pointer
+ * @to: string to make set location
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
