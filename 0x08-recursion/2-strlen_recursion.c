cc8a3d9c037a6c6da84f5be435a919a06517fffa
Codackblue <goodyokorafor1@gmail.com>
2023-09-28 10:43:38 -0700

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - Returns the value to longit
+ * @s: The recursion string longit
+ *  Return: The length of the longit string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+}
